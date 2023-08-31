eac13f1cdbf1a8a66bfe8237f19436c829eae547
rejaul <devrejaul.official@gmail.com>
2023-08-31 23:26:30 +0600

    add

---
+++ 11.python-type-conversion/app.py
@@ -0,0 +1,24 @@
+# combine string and a number 
+# x= 21
+# text = 'my favorite number is '+x
+# print(text);  # print error
+
+# correct way 
+# convert number to string
+y = 20 
+text2 = "My favorite number is " + str(y);  # number to string
+print(text2); 
+
+
+# convert to integer 
+x = 4.45; 
+x= int(x); 
+print(x)
+
+
+# convert to float 
+x= 4 ; 
+x = float(x); 
+print(x); 
+
+
---
+++ 12.python-booleans/app.py
@@ -0,0 +1,18 @@
+a =True
+b = False
+
+print(a); 
+print(b); 
+
+print(5==5); 
+
+age = 20
+
+if(age>18):
+    print("Yor are to alow to enter"); 
+
+# checking type of boolean
+x = True; 
+y = type(x)
+
+print(y)
