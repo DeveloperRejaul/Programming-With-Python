2e418eec2330926c53733b7dc54632f6e75556d0
rejaul <devrejaul.official@gmail.com>
2023-09-04 01:18:59 +0600

    add python oprator

---
+++ 13.paython-arithmetic-oprator/app.py
@@ -0,0 +1,60 @@
+# python arithmetic operator
+# 1. Addition operator +
+# 2. subtraction  operator -
+# 3. Multiplication  operator *
+# 4. Division operator /
+# 5. Exponentiation operator **
+# 6. Remainder operator %
+# 7. Floor Division operator //
+
+# addition operator
+x = 10
+y = 20
+
+z = x+y
+print(z)
+
+# Subtraction Operator
+x = 10
+y = 5
+z = x-y
+print(z)
+
+
+# Division Operator
+x = 10
+y = 2
+z = x/y
+print(z)
+
+# multiplication operator
+x = 10
+y = 2
+z = x * y
+print(z)
+
+
+# Exponentiation Operator
+x = 2**3  # 2*2*2
+print(x)
+
+# Remainder Operator
+x = 9
+y = 2
+
+z = x % 2
+print(z)
+
+
+# Floor Division Operator
+x = 5 // 2  # 2 , note 2.5
+print(x)
+
+
+# Operator Sequence
+x = 10+3*5  # 25
+print(x)
+
+
+y = (10+3)*5  # 65
+print(y)
